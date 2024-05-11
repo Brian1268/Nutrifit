462b3717b052e735b170c9bb4d56d340a20f6262
Brian1268 <brianipad2003@gmail.com>
2024-05-11 10:07:40 +0530

    delete

--- JS/fetcherHeaderLogin.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Fetch the header.html content using fetch API
-  fetch("Header-login.php")
-    .then((response) => response.text())
-    .then((data) => {
-      // Insert the fetched HTML into the #header div
-      document.getElementById("Header-login").innerHTML = data;
-    });
-});
